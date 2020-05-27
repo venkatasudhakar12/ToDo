8a94a4bc11b1c155d1713a5d1c4366faeedbc269
Venkata Sudhakar <venkatasudhakar33@gmail.com>
2020-05-27 11:02:16 +0530

    Initial Commit

---
+++ ToDo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ToDo
+//
+//  Created by venkata sudhakar on 27/05/20.
+//  Copyright © 2020 venkata sudhakar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
